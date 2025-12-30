d908612d7c344aeed84c0cad6aa24d8244846622
Furqan <furqanlatif615@gmail.com>
2025-12-30 13:40:44 +0500

    Data services initiate

--- src/app/(root)/data-services/data-cabling/page.tsx
+++ src/app/(root)/data-services/data-cabling/page.tsx
@@ -1,3 +1,5 @@
+import { Shield } from "lucide-react";
+import MainCTA from "../../_components/cta";
 import HeroBanner from "../../_components/HeroBanner";
 import WhyChooseKinetic from "../../_components/why-us";
 import DataCablingServices from "./_components/DataCablingServices";
@@ -15,7 +17,17 @@ export default function DataCabling() {
       />
       <DataCablingServices />
       <WhyChooseKinetic />
-      <IndustriesWeServe/>
+      <IndustriesWeServe />
+      <MainCTA
+        subtitle="Connect with Confidence"
+        title="Reliable Data Cabling Solutions"
+        description="Ensure high-speed, secure, and scalable network connectivity for your offices, data centers, and industrial facilities with our expert cabling services."
+        buttonText="Request a Consultation"
+        buttonLink="/contact"
+        buttonIcon={<Shield className="w-5 h-5" />}
+        bgImage="/assets/images/mainCtaBg.webp"
+        className="mt-16 mb-20 mx-6"
+      />
     </div>
   );
 }
